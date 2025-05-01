a41e6f5879bb395e24af0660c5d6e83fa16f99e4
Daria Yats <dariayatsyniuk@gmail.com>
2025-05-01 11:48:30 +0200

    initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Дарья Яцынюк on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
